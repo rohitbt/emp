0b34d58d1f0622f59959250f7fb01e0aea32d59b
rohitbt <40511303+rohitbt@users.noreply.github.com>
2018-10-28 15:33:33 +0530

    Update empdemo.php

--- empdemo.php
+++ empdemo.php
@@ -1,40 +1,24 @@
 <?php
-$requestBody = file_get_contents('php://input');
-$json = json_decode($requestBody);
+header('Content-Type: application/json');
 
-$equis = $json->result->parameters->equis;
-
-switch ($equis) {
-    case 'hi':
-        $speech = "Hi, Nice to meet you";
-
-        break;
-
-    case 'bye':
-        $speech = "Bye, good night";
-        break;
-
-    case 'anything':
-        $speech = "Yes, you can type anything here.";
-        break;
-
-    default:
-        $speech = "Sorry, I didnt get that. Please ask me something 
-else.";
-        break;
+function processMessage($update) {
+    if($update["result"]["action"] == "sayHello"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
+    }
 }
 
-
-$response = new \stdClass();
-$response->speech = $speech;
-$response->displayText = $speech;
-$response->source = "Alex";
-
-echo json_encode($response);
-}
-else
-{
-echo "Method not allowed";
+function sendMessage($parameters) {
+    echo json_encode($parameters);
 }
 
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
 ?>
